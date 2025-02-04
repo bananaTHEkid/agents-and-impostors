02bbb5c636d2938a64400be7da85f75294bb8290
Tahir Akpinar <tahir111@outlook.de>
2025-02-05 00:12:23 +0100

    working on first POST

--- app/components/login.tsx
+++ app/components/login.tsx
@@ -1,17 +1,55 @@
 import Head from "./Header";
 import { NavLink } from "react-router";
+import { useEffect, useState } from "react";
+import { setegid } from "process";
 
 export function Login() {
+
+  const [data, setData] = useState(null);
+  const [name, setName] = useState("");
+  const [lobbyCode, setlobbyCode] = useState("");
+
+  const postData = () => {
+    fetch("http://localhost:3000/lobby/create-lobby", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name, lobbyCode }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Response data: ", data);
+        setData(data);
+      })
+      .catch((error) => console.error("Error posting data:", error));
+  };
+
   return (
     <main className="flex items-center justify-center pt-16 pb-4">
       <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
         <Head />
         <div className="max-w-[300px] w-full space-y-6 px-4">
-          <input type="text" placeholder="name"></input>
-          <input type="text" placeholder="Room Code"></input>
+
+          <input 
+            type="text" 
+            placeholder="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          ></input>
+
+          <input 
+            type="text" 
+            placeholder="lobbyCode"
+            value={lobbyCode}
+            onChange={(e) => setlobbyCode(e.target.value)}
+          ></input>
+
           <div className="grid grid-cols-2 gap-4">
             <NavLink to="/lobby">
-              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" >
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" 
+              onClick={postData}
+              >
                 Create Room
               </button>
             </NavLink>
@@ -22,7 +60,9 @@ export function Login() {
               </button>
             </NavLink>
           </div>
+          
         </div>
+        
       </div>
     </main>
   );
